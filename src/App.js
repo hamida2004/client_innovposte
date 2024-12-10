48d13cfc09770fade330c100dc7881f56758c3c2
Dadda Hamida <111181409+hamida2004@users.noreply.github.com>
2024-12-10 11:28:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import SDA from "./screens/SDA";
 import Idees from "./screens/Idees";
